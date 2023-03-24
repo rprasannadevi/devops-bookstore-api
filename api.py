b9d7e8ff32815cde1c7acbe48f44ae0007a37757
Prasanna <prasannadevi@gmail.com>
2023-03-24 15:08:39 +0000

    Added Prometheus data for metrics

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+from prometheus_flask_exporter import RESTfulPrometheusMetrics
 from flask import Flask
 from flask_restful import Resource, Api, reqparse, abort, marshal, fields
 from flask_cors import CORS
@@ -6,6 +7,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
+
+metrics = RESTfulPrometheusMetrics(app, api)
+
+metrics.info('app_info', 'Application info', version='1.0', app_name='devops-bookstore-api')
+
 
 # A List of Dicts to store all of the books
 books = [{
@@ -47,8 +53,11 @@ class BookList(Resource):
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
 
+    @metrics.summary('requests_by_status', 'Request latencies by status', labels={'status': lambda r: r.status_code})
     def get(self):
-        return{"books": [marshal(book, bookFields) for book in books]}
+        return{
+            "books": [marshal(book, bookFields) for book in books]
+            }, 200
 
 
 api.add_resource(BookList, "/books")
